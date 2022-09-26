de82f76927883ce3f6fe8bf1393d1785c3f5f4ab
Hilario Iñigo Menendez Picossi <32109140+hpicossi@users.noreply.github.com>
2022-09-26 17:23:25 -0300

    Nueva modificacion

--- biblioteca-backend/src/main/java/com/libreria/biblioteca/service/LendingsService.java
+++ biblioteca-backend/src/main/java/com/libreria/biblioteca/service/LendingsService.java
@@ -1,4 +1,28 @@
 package com.libreria.biblioteca.service;
 
-public class LendingsService {
+import com.libreria.biblioteca.models.Lendings;
+import com.libreria.biblioteca.repository.LendingsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class LendingsService implements ILendingsService {
+
+    @Autowired
+    public LendingsRepository lendingsRepository;
+
+    @Override
+    public List<Lendings> traerLendings(){
+        return lendingsRepository.findAll();
+    }
+
+    @Override
+    public void crearLendings(Lendings lendings){
+        lendingsRepository.save(lendings);
+    }
+
+    @Override
+    public void bajarLendings(Integer id){
+        lendingsRepository.deleteById();
+    }
 }
--- biblioteca-backend/src/main/java/com/libreria/biblioteca/service/UsersService.java
+++ biblioteca-backend/src/main/java/com/libreria/biblioteca/service/UsersService.java
@@ -1,4 +1,28 @@
 package com.libreria.biblioteca.service;
 
-public class UsersService {
+import com.libreria.biblioteca.models.Users;
+import com.libreria.biblioteca.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class UsersService implements IUsersService{
+
+    @Autowired
+    public UsersService usersService;
+
+    @Override
+    public List<Users> traerUsers(){
+        return UsersRepository.findAll();
+    }
+
+    @Override
+    public void crearUsers(Users users){
+        UsersRepository.save(users);
+    }
+
+    @Override
+    public void bajarUsers(Integer id){
+        UsersRepository.deleteById();
+    }
 }
