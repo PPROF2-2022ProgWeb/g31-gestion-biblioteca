8ee30f0109b708968727605c8847a1b39d5574de
wesborland-github <MarcosJavierGomezH@gmail.com>
2022-09-27 18:19:50 -0300

    agregadas funciones para guardar datos de formularios

--- biblioteca-frontend/src/app/component/nuevo-usuario/nuevo-usuario.component.ts
+++ biblioteca-frontend/src/app/component/nuevo-usuario/nuevo-usuario.component.ts
@@ -12,7 +12,10 @@ export class NuevoUsuarioComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  
+  onClickSubmit(data: { inputNombre: string; inputDireccion: string; inputApellido: string; inputTelefono: string; inputApellido1: string }) {
+    alert("Nombre : " + data.inputNombre + "Direccion : " +data.inputDireccion + "Apellido 1 : " + data.inputApellido + "Telefono : " +data.inputTelefono + "Apellido 2 : " +data.inputApellido1);
+ }
+
   
 
 }
